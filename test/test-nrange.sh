dad9af016e920e29cf6caeb6c2adb2fb451dbb8c
Alexander Morozov <Alexander.Morozov@itiviti.com>
2022-04-18 15:22:02 +0200

    Add test cases

--- test/test-nrange.sh
+++ test/test-nrange.sh
@@ -6,5 +6,6 @@ for arg do
     STR=`echo $arg | sed -e 's/.*nrange-//' | sed -e 's/.txt.eta//'`
     RANGE=`echo $STR | sed -e 's/[0-9]\{1,\}-//'`
     N=`echo $STR | sed -e 's/-.*//'`
-    $CMD --head-count=${N} --input-range=$RANGE | sort | diff -u --from-file ${arg} - || exit 1
+    echo "$CMD --head-count=$N --input-range=$RANGE"
+    $CMD --head-count=$N --input-range=$RANGE | sort | diff -u --from-file $arg - || exit 1
 done
--- test/test-range.sh
+++ test/test-range.sh
@@ -4,5 +4,6 @@ CMD=$1
 shift
 for arg do
     RANGE=`echo $arg | sed -e 's/.*range-//' | sed -e 's/.txt.eta//'`
-    $CMD -i $RANGE | sort | diff -u --from-file ${arg} - || exit 1
+    echo "$CMD -i $RANGE"
+    $CMD -i $RANGE | sort | diff -u --from-file $arg - || exit 1
 done
